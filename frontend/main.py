e9930ed4fd29786e0f7bb2efc62f80ecf4187d12
DavidAlexanderMoe <Davidalexander100@hotmail.com>
2023-04-23 16:11:46 +0200

    interaction with search-nlu working

--- frontend/main.py
+++ frontend/main.py
@@ -4,6 +4,11 @@
 # oper PS and go into frontend directory with cd frontend
 # run the main file: see the url where it is running
 # wee need to make it prettier and let the user be able to use it
+# create a new route that manages the interaction with the html form
+# use requests module to call api and get info on url
+# open second terminal, cd search-nlu, python app.py, copy url at which is running
+# insert the url in the .post request, add /search whcih is the endpoint
+#
 
 
 
@@ -14,6 +19,12 @@ load_dotenv()
 # IMPORT DEPENDENCIES
 import json, os
 from flask import Flask, request, render_template
+import requests
+
+# with this the app can work also in other workstations
+# make the url a parameter to define port
+# ADD IT TO THE .ENV FILE IN TEMPLATES
+SEARCH_NLU_ENDPOINT = os.getenv("SEARCH_NLU_ENDPOINT")
 
 # CREATE FLASK SERVER
 app = Flask("frontend-flask")
@@ -22,6 +33,30 @@ app = Flask("frontend-flask")
 @app.route("/", methods=["GET"])
 def homepage():
     return render_template("index.html")
+
+@app.route("/analyze", methods=["POST"])             # the /analyze should be the same as the one you put in the html <form>
+def search():
+    print("Received call to /search endpoint")
+    # collecting parameters
+    url_to_search = request.form["webpage"]          # read content of the request
+    print("url to search:", url_to_search)
+    # send request to nlu backend (search-nlu)
+    payload = json.dumps({"url": url_to_search})
+    headers = {'Content-type': 'application/json'}
+    nlu_results_raw = requests.request("POST", SEARCH_NLU_ENDPOINT, headers=headers, data=payload)
+    nlu_results = nlu_results_raw.json()
+    print(nlu_results)
+    # return results
+    # for example: return "<h1>Results</h1><p>... SOME RESULTS ...</p>"
+    results_page = "<h1>Results</h1>"
+    results_page += "<p>Within the selected webpage I have extracted the following kewords:</p>"
+    results_page += "<p>"                       # open the section
+    for keyword in nlu_results['results:']:
+        results_page += keyword + "</br>"       # add new line with breakpoint
+    results_page += "</p>"                      # close the paragraph
+    results_page += "<a href=\"/\">Return to homepage</a>"                  # add html link
+    # by adding \/ you get that the "" are part of the string
+    return results_page
 
 # health check
 @app.route("/health", methods=["GET"])
